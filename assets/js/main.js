37628d6f163e2fee635df4a1e6637e9005c36bf5
Shams425 <alexdavid486@gmail.com>
2022-08-16 19:56:18 +0200

    setting the pages carsual and pages numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
+//owl carousal library for sliding pages
+$(function () {
+  $("#pagesContainer").owlCarousel({
+    loop: false,
+    margin: 30,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+    },
+  });
+
+  //setting the page number
+  const pageNumbers = document.querySelectorAll(".owl-dot");
+
+  pageNumbers.forEach((page, index) => {
+    page.textContent = index + 1;
+  });
+
+  console.log(pageNumbers);
+});
+
 //making all the Contents chapters in same length
 const chaptersTitle = document.querySelectorAll(".accordion h2");
 const contents = document.querySelectorAll(".accordion-body li a");
@@ -20,5 +45,3 @@ contents.forEach((content) => {
   }
   return content;
 });
-
-console.log(contentsList[0].clientWidth);
