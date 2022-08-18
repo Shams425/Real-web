3eb8d13b7a158ed30b7a0c76f7e9a0abbed7688a
Shams425 <alexdavid486@gmail.com>
2022-08-18 05:46:07 +0200

    fix the position the page numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,38 +20,58 @@ $(function () {
   const navButtonNext = document.querySelector(".owl-nav .owl-next");
   const navButtonBack = document.querySelector(".owl-nav .owl-prev");
 
-  let nowIndex = 0;
+  let currentIndex = 0;
 
   pageNumbers.forEach((page, index) => {
     page.textContent = index + 1;
   });
 
-  navButtonNext.addEventListener("click", (e) => {
+  navButtonNext.addEventListener("click", () => {
+    currentIndex++;
+
     pageNumbers.forEach((page) => {
-      if (page.classList.contains("active")) {
-        nowIndex = parseInt(page.textContent);
-        pageNumbers[nowIndex - 1].style.display = "block";
-      } else {
-        page.style.display = "none";
-      }
-      page.style.left = `-${page.clientWidth * nowIndex}px`;
+      page.classList.remove("showDots");
     });
-    console.log(nowIndex);
+
+    if (currentIndex < pageNumbers.length - 3) {
+      pageNumbers[currentIndex - 1].classList.add("active");
+      pageNumbers[currentIndex].classList.add("standBy");
+      pageNumbers[currentIndex + 1].classList.add("standBy");
+      pageNumbers[pageNumbers.length - 1].style.left = "30px";
+      pageNumbers[pageNumbers.length - 1].classList.add("active");
+      pageNumbers[currentIndex + 1].classList.add("showDots");
+    } else {
+      pageNumbers[currentIndex - 1].classList.add("active");
+      pageNumbers[currentIndex].classList.add("active");
+      pageNumbers[currentIndex + 1].classList.add("active");
+      pageNumbers[pageNumbers.length - 1].style.left = "0px";
+      pageNumbers[pageNumbers.length - 1].classList.add("active");
+    }
   });
 
   navButtonBack.addEventListener("click", () => {
+    currentIndex--;
+    if (currentIndex <= 1) currentIndex = 1;
+
     pageNumbers.forEach((page) => {
-      if (page.classList.contains("active")) {
-        nowIndex = parseInt(page.textContent);
-        pageNumbers[nowIndex].style.display = "block";
-      } else {
-        page.style.display = "none";
-      }
-      pageNumbers.forEach((page) => {
-        page.style.left = `-${page.clientWidth * nowIndex}px`;
-      });
+      page.classList.remove("showDots");
+      page.classList.remove("standBy");
     });
-    console.log(nowIndex);
+
+    if (currentIndex < pageNumbers.length - 3) {
+      pageNumbers[currentIndex - 1].classList.add("active");
+      pageNumbers[currentIndex].classList.add("standBy");
+      pageNumbers[currentIndex + 1].classList.add("standBy");
+      pageNumbers[pageNumbers.length - 1].style.left = "30px";
+      pageNumbers[pageNumbers.length - 1].classList.add("standBy");
+      pageNumbers[currentIndex + 1].classList.add("showDots");
+    } else {
+      pageNumbers[currentIndex - 1].classList.add("active");
+      pageNumbers[currentIndex].classList.add("active");
+      pageNumbers[currentIndex + 1].classList.add("active");
+      pageNumbers[pageNumbers.length - 1].style.left = "0px";
+      pageNumbers[pageNumbers.length - 1].classList.add("active");
+    }
   });
 });
 
